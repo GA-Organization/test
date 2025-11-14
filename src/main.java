152c9938dc769ec742dca7430209ec10c43a6fbe
gerryaris <56064071+gerryaris@users.noreply.github.com>
2025-11-14 14:22:44 +0700

    Add getUserHibernate method to Main class
    Added a method to retrieve a user from the database using Hibernate.

--- src/main.java
+++ src/main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     } 
+
+    public User getUserHibernate(org.hibernate.Session session, String data) {
+    
+      org.hibernate.Query query = session.createQuery(
+                "FROM students where fname = " + data);  // Sensitive
+      // ...
+    }
 }
